e3294d9d29000bebacde5bf0de5794b4ca3373d9
Wang <vincent.wang@cgi.com>
2018-01-29 12:07:24 -0500

    test add.
    t push -u origin master
    t
    commit
    ;
    add

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,4 +3,5 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
+//here
 module.exports = {mongoose};
